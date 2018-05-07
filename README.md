a9ecac13309244b193eafcab76c72d2236a87835
badc0d3 <badc0d3@outlook.com>
2018-05-07 09:53:17 -0400

    Add PHP cmd file template

--- README.md
+++ README.md
@@ -13,14 +13,14 @@ usage: createWebShell.py [-h] -t File_Type -ip ip_Address [-p Port] -o Output_Fi
 
 Create WebShell for Education Purpose ONLY.
 
-required arguments:
-  -t File_Type    WebShell file type, options: php, asp, aspx
-  -ip ip_Address  Enter local host IP Address
-  -o Output_File  Enter the name of the output file
-
 optional arguments:
   -h, --help      show this help message and exit
   -p Port         Enter local host PORT, Default: 8080
+
+required arguments:
+  -t File_Type    WebShell file type, options: php, php-cmd, asp, aspx
+  -ip ip_Address  Enter local host IP Address
+  -o Output_File  Enter the name of the output file
 
 Ex: python createWebShell.py -t php -ip 10.10.10.10 -p 8080 -o myshell.php
 ```
--- createWebShell.py
+++ createWebShell.py
@@ -6,35 +6,37 @@ parser = argparse.ArgumentParser(
 		description='Create WebShell for Education Purpose ONLY.',
 		epilog='Ex: python createWebShell.py -t php -ip 10.10.10.10 -p 8080 -o myshell.php')
 
-parser.add_argument('-t',
-		metavar='File_Type',
-		dest='TYPE',
-		required='true',
-		help='WebShell file type, options: php, asp, aspx')
+requiredparser = parser.add_argument_group('required arguments')
+requiredparser.add_argument('-t',
+                metavar='File_Type',
+                dest='TYPE',
+                required='true',
+                help='WebShell file type, options: php, php-cmd, asp, aspx')
 
-parser.add_argument('-ip',
-		metavar='ip_Address',
-		dest='IP',
-		required='true',
-		help='Enter local host IP Address')
+requiredparser.add_argument('-ip',
+                metavar='ip_Address',
+                dest='IP',
+                required='true',
+                help='Enter local host IP Address')
+
+requiredparser.add_argument('-o',
+                metavar='Output_File',
+                dest='OUTPUT',
+                required='true',
+                help='Enter the name of the output file')
 
 parser.add_argument('-p',
-		metavar='Port',
-		dest='PORT',
-		default='8080',
-		help='Enter local host PORT, Default: 8080')
-
-parser.add_argument('-o',
-		metavar='Output_File',
-		dest='OUTPUT',
-		required='true',
-		help='Enter the name of the output file')
+                metavar='Port',
+                dest='PORT',
+                default='8080',
+                help='Enter local host PORT, Default: 8080')
 
 args = parser.parse_args()
 
 def f(x):
     return {
         'php': 'rs.php',
+	'php-cmd' : 'cmd.php',
         'asp': 'cmd.asp',
 	'aspx' : 'cmd.aspx'
     }[x]
@@ -49,3 +51,5 @@ for s in input.xreadlines(  ):
 
 output.close()
 input.close()
+
+print '[+] ', args.OUTPUT ,' File was created'
